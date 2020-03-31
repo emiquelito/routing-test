854a10619ec9e4568e7a2a3876fb9f7a48b435f7
Evandro Miquelito <98694+emiquelito@users.noreply.github.com>
2020-03-31 17:38:17 -0600

    Update server.py

--- server.py
+++ server.py
@@ -12,4 +12,4 @@ def about():
     return '<body>This is the about page</body>'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3300, debug=True)
